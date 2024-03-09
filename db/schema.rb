2d022b1bc9cf22e09c0f7a2f4299d4a04dfdc60a
Brigham <brig.johnson@gmail.com>
2024-03-08 23:21:05 -0500

    set up category table

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_03_09_040325) do
+ActiveRecord::Schema[7.1].define(version: 2024_03_09_041702) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "categories", force: :cascade do |t|
+    t.text "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "expenses", force: :cascade do |t|
     t.text "title"
