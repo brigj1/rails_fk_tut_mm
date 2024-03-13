a76fb36373e6e05a9617e8e5235d83033de835d3
Brigham <brig.johnson@gmail.com>
2024-03-12 23:59:11 -0400

    creating new Category objects should populate 'id' with value of type 'UUIDv7'

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,3 +1,14 @@
 class Category < ApplicationRecord
+  before_create :generate_uuid_v7
+
   has_many :expenses
+
+  private
+
+    def generate_uuid_v7
+           
+      # NOTE: self.class.attribute_types['id'].type is nil at this point
+
+      self.id ||= SecureRandom.uuid_v7
+    end
 end
