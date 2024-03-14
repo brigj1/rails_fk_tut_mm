ac7a4f925a729f288d732da235580c60c5aff2bf
Brigham <brig.johnson@gmail.com>
2024-03-13 22:07:05 -0400

    for each model, initialize primary key value for 'id' as UUIDv7

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,19 +1,8 @@
 class Category < ApplicationRecord
-  before_create :generate_uuid_v7
-
   has_many :expenses
 
   def to_param
     return nil unless persisted?
     id.to_s
   end
-
-  private
-
-    def generate_uuid_v7
-           
-      # NOTE: self.class.attribute_types['id'].type is nil at this point
-
-      self.id ||= SecureRandom.uuid_v7
-    end
 end
